6bb48110d1b65eb3c113cb56de36021d24baba71
juegofree <frcampuzanoca@uide.edu.ec>
2023-12-06 15:49:46 -0500

    mi primer commit

--- clase1.py
+++ clase1.py
@@ -13,3 +13,19 @@ c= 7
 print(f"el numero {a} es el primer valor impar, el segundo es {b} y el tercero es {c}")
 print(a+b)
 print(f" el numero {a}, es menor que {b}")
+
+print(f"la sumatoria de {a},{b},{c}={a+b+c}")
+
+#ejemplo de entero
+x=3
+#ejemplo de flotante
+y=2.5
+#ejemplo de string
+bienvenida= "Hola como estas?\n te damos la bienvenida a este codigo de programcaión con Python"
+#ejemplo de boolean
+bo=True
+
+valor1= int(input("Ingrese el primer valor: "))
+valor2= float(input("Ingrese el segundo valor: "))
+
+print("el resultado de la suma es:", valor1+valor2)
