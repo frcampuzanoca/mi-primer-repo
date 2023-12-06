afb923dbc55910970d45eca44f16ba5910072523
juegofree <frcampuzanoca@uide.edu.ec>
2023-12-06 13:41:15 -0500

    mi primer commit

---
+++ clase1.py
@@ -0,0 +1,15 @@
+a= "bienvenidos a mi 'primer' programa"
+b= 'En mi lenguaje de programacion "python"'
+
+print(a+" "+b)
+b="con el uso de python"
+
+print(a,b, "gracias")
+
+a= 3
+b= 5
+c= 7
+
+print(f"el numero {a} es el primer valor impar, el segundo es {b} y el tercero es {c}")
+print(a+b)
+print(f" el numero {a}, es menor que {b}")
