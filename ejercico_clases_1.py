3cfef5553f567643e689eca50c0188fe1943263c
juegofree <frcampuzanoca@uide.edu.ec>
2023-12-10 20:22:12 -0500

    mi primer commit

--- ejercico_clases_1.py
+++ ejercico_clases_1.py
@@ -1,12 +1,5 @@
-valor1= int(input("Ingrese el primer numero: "))
-valor2= int(input("Ingrese el segundo valor: "))
-valor3= int(input("ingrese el tercer valor: "))
-
-if valor1>valor2 and valor1>valor3:
-    mayor= valor1
-elif valor2>valor1 and valor2>valor3:
-    mayor= valor2
-elif valor3>valor1 and valor3>valor2:
-    mayor= valor3
-
-print(f"El numero mayor es:{mayor}")
+i=0
+while i<10:
+    print(i)
+    i=i+2
+print("fin de bucle")
